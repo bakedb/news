2af6ae25dc6f91b2d44b2f5373dadffb984950c9
bkd <contact@bkd.lol>
2026-08-27 20:45:20 -0500

    fixed incorrect redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Pass post files into a "posts" collection sorted by date
-  eleventyConfig.addCollection("posts", function(collectionApi) {
+  eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
   });
 
   return {
+    pathPrefix: "/news/",
     dir: {
       input: "src",
       output: "_site"
